77d57962d0d9bdf751bff32430622483276097d2
danieljackson31@hotmail.com <47636562+Ped4ntic@users.noreply.github.com>
2023-09-12 16:20:35 +0100

    check in 2nd set

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    const str = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return str.split("").reverse().join("") === str;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((a, b) => {
+        const currentDate = new Date();
+        const cur = (a.yearOfDeath || currentDate.getFullYear()) - a.yearOfBirth;
+        const pre = (b.yearOfDeath || currentDate.getFullYear()) - b.yearOfBirth;
+        return cur > pre ? a : b;
+    });
 };
 
 // Do not edit below this line
